0cda1ecbc14734de7ea708fbb315572108ae61ec
Yanrong Ji <jerryji1993@gmail.com>
2020-08-16 14:05:12 -0500

    Update motif_utils.py

--- motif/motif_utils.py
+++ motif/motif_utils.py
@@ -22,6 +22,22 @@ def kmer2seq(kmers):
     assert len(seq) == len(kmers_list) + len(kmers_list[0]) - 1
     return seq
 
+def seq2kmer(seq, k):
+    """
+    Convert original sequence to kmers
+    
+    Arguments:
+    seq -- str, original sequence.
+    k -- int, kmer of length k specified.
+    
+    Returns:
+    kmers -- str, kmers separated by space
+
+    """
+    kmer = [seq[x:x+k] for x in range(len(seq)+1-k)]
+    kmers = " ".join(kmer)
+    return kmers
+
 def contiguous_regions(condition, len_thres=5):
     """
     Modified from and credit to: https://stackoverflow.com/a/4495197/3751373
